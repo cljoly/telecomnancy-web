43968fada56b5c5f9aedc4c1b0f44b804f670eb9
JOLY Clément <clement.joly@telecomnancy.eu>
2019-04-03 08:52:35 +0200

    Fin de la base de donnée

--- db/db_objects.py
+++ db/db_objects.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python3
 from main import db
+from sqlalchemy.orm import relationship
 
-# Utilisateur élève
+# Utilisateur élève ou enseignants.
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(80), unique=False, nullable=False)
     name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
-    password = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
     salt = db.Column(db.String(120), unique=True, nullable=False)
 
     gitlab_username = db.Column(db.String(80), unique=True, nullable=False)
 
-# Utilisateur enseignant
+# Propriétés propres à un utilisateur enseignant
 class Teacher(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(80), unique=False, nullable=False)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    password = db.Column(db.String(120), unique=True, nullable=False)
-    salt = db.Column(db.String(120), unique=True, nullable=False)
-
-    gitlab_username = db.Column(db.String(80), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = relationship('User')
     # API key
-    gitlab_key = db.Column(db.String(80), unique=True, nullable=False)
+    gitlab_key = db.Column(db.String(80), unique=False, nullable=False)
 
 # Matière : POO, CSD…
 class Unit(db.Model):
@@ -36,8 +31,33 @@ class Unit(db.Model):
     short = db.Column(db.String(10), unique=True, nullable=False)
     # Enseignant responsable
     in_charge_id = db.Column(db.Integer, db.ForeignKey('teacher.id'))
+    in_charge = relationship("Teacher")
 
-# Année scolaire, 2018 par exemple
-class Year(db.Model):
+# Par exemple, TP1
+class Activity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    value = db.Column(db.Integer, unique=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    year = db.Column(db.Integer)
+
+# Groupe d’élèves ou de prof travaillant sur un dépôt, éventuellement d’une
+# seule personne.
+class UserSet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # Nom du groupe, par exemple G1, IL ou Memscarlo
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+# Chaque élève appartient à plusieurs groupes
+class UserSetUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = relationship("User")
+    user_set_id = db.Column(db.Integer, db.ForeignKey('user_set.id'))
+    user_set = relationship("UserSet")
+
+# Dépot git particulier
+class Repo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(120), unique=False, nullable=False)
+    # Groupe d’élève en charge du dépot en charge du dépôt
+    userset_id = db.Column(db.Integer, db.ForeignKey('user_set.id'))
+    userset = relationship("UserSet")
--- test_db.py
+++ test_db.py
@@ -8,9 +8,9 @@ from db_objects import User
 db.create_all()
 
 # Quelques tests
-u1 = User(firstname="Victor", name="Hugo", email="victor@hugo.me", gitlab_username="hugo1802u")
-u2 = User(firstname="Jules-Amédée", name="Barbey d’Aurevilly", email="jules_amedee_barbey_daurelvilly@telecomnancy.univ-lorraine.fr", gitlab_username="1808u")
-u3 = User(firstname="Louis", name="de Rouvroy de Saint-Simon", email="louis-de-rouvroy-de-saint-simon@grand-duc-et-pair-de-france.fr", gitlab_username="derouvroydesaintsimon1675u")
+u1 = User(firstname="Victor", name="Hugo", email="victor@hugo.me", password="auie", salt="a", gitlab_username="hugo1802u")
+u2 = User(firstname="Jules-Amédée", name="Barbey d’Aurevilly", email="jules_amedee_barbey_daurelvilly@telecomnancy.univ-lorraine.fr", password="auie", salt="b", gitlab_username="1808u")
+u3 = User(firstname="Louis", name="de Rouvroy de Saint-Simon", email="louis-de-rouvroy-de-saint-simon@grand-duc-et-pair-de-france.fr", password="auie", salt="c", gitlab_username="derouvroydesaintsimon1675u")
 user_list = [ u1, u2, u3 ]
 for user in user_list:
     db.session.add(user)
