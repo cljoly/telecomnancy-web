65bc57049a8a4c84777ae29a794a1a11aeb04af0
lucas <lucas.fenouillet@telecomnancy.eu>
2019-04-25 10:33:32 +0200

    mise en forme

--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
 from math import ceil
 import time
-from database.db_objects import Activity,Repository
+from database.db_objects import Activity, Repository
 from main import db
 from sqlalchemy import func
 
@@ -31,8 +31,8 @@ class Pagination(object):
         last = 0
         for num in range(1, self.pages + 1):
             if num <= left_edge or \
-               (self.page - left_current - 1 < num < self.page + right_current) or \
-               num > self.pages - right_edge:
+                    (self.page - left_current - 1 < num < self.page + right_current) or \
+                    num > self.pages - right_edge:
                 if last + 1 != num:
                     yield None
                 yield num
@@ -40,14 +40,24 @@ class Pagination(object):
 
 
 class Group:
+    """Regroupe les informations correspondantes à une activitée"""
+
     def __init__(self, name, count, c_date=time.strftime("%d/%m/%Y"), d_date="None", link='/activity'):
-        self.c_date = c_date                        # the name of the activity
-        self.name = name                            # date of creation
-        self.count = count                          # due date
-        self.d_date = d_date                        # nb of members
-        self.link = link                            # link to the activity page
+        self.c_date = c_date  # the name of the activity
+        self.name = name  # date of creation
+        self.count = count  # due date
+        self.d_date = d_date  # nb of members
+        self.link = link  # link to the activity page
+
 
 def get_activities_for_page(page, count):
-    #result = Activity.query.filter(Activity.id.between((page - 1) * PER_PAGE, min((page - 1) * PER_PAGE + PER_PAGE, count)))
-    result = db.session.query(Activity.name, func.count(Repository.id), Activity.start_date, Activity.end_date).filter(Activity.id == Repository.activity_id).group_by(Activity.name)
-    return [Group(result[i].name, result[i].count, result[i].start_date, result[i].end_date) for i in range((page - 1) * PER_PAGE, min((page - 1) * PER_PAGE + PER_PAGE, count))]
+    """Effectue les requêtes necessaires pour récupérer les infos de Activities correspondantes à la page page"""
+    result = db.session.query(
+        Activity.name, func.count(Repository.id), Activity.start_date, Activity.end_date
+    ).filter(
+        Activity.id == Repository.activity_id
+    ).group_by(
+        Activity.name
+    )
+    return [Group(result[i].name, result[i].count, result[i].start_date, result[i].end_date)
+            for i in range((page - 1) * PER_PAGE, min((page - 1) * PER_PAGE + PER_PAGE, count))]
